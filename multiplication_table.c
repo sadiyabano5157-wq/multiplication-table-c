ca56fc1eec8b35b921c4714c8594e2e1b7f070af
sadiyabano5157-wq <sadiyabano5157@gmail.com>
2026-05-17 09:07:57 +0530

    Add multiplication table program in C

---
+++ multiplication_table.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int i, j;
+
+	for(i = 1; i <= 5; i++)
+	{
+		printf("Multiplication table of %d\n", i);
+
+		for(j = 1; j <= 10; j++)
+		{
+			printf("%d * %d = %d\n", i, j, i*j);
+		}
+
+		printf("\n");
+	}
+
+	return 0;
+}
